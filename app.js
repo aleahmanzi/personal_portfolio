4b9cd2cf0f132bde6e3bf16ee03fdacb09d97cb5
Aleah Manzi <aleah.manzi@gmail.com>
2016-09-06 18:13:19 -0400

    successful call to API

--- app.js
+++ app.js
@@ -21,14 +21,12 @@ var demo = angular.module('demo',['ngRoute']);
 demo.controller('homectrl', function($scope, $http){
   
   $scope.recipeList = [];
-  $scope.appKey = "";
-  $scope.appID = "";
 
   /// - default values
 
  $scope.search = function(term) {
   console.log("working!")
-  $http ({ url: 'http://api.yummly.com/v1/api/recipes?appID&appKey&' + term, method: 'JSONP', params: {callback: 'JSON_CALLBACK'} })
+  $http.get('http://api.yummly.com/v1/api/recipes?_app_id=9fb53b76&_app_key=d2fd726c3304d520c0ede8036c93f010&maxResult=50&q=' + term)
    .success(function(result){
    	 console.log(result);
    	 $scope.recipeList = result;
