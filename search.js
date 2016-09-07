2902373c3311ca492db9b7107ecf0bbaf10bd613
Aleah Manzi <aleah.manzi@gmail.com>
2016-09-06 21:40:07 -0400

    fixed factory and updated API call.

--- search.js
+++ search.js
@@ -2,26 +2,19 @@ angular.module('myApp').controller('searchctrl',
 	function($scope, search){
 
   $scope.recipeList = [];
+  $scope.matches = [];
   /// - default values
 
-
-  search().success(function(result){
-  	 console.log(result);
-   	 $scope.recipeList = result;
-   });
+   $scope.search = function(){
+    console.log($scope.term);
+     search($scope.term).success(function(result){
+      console.log(result);
+      $scope.matches = result.matches;
+     })
+   };
 
 });
 
 
-
-
- /*$scope.search = function(term) {
-  console.log("working!")
-  $http.get('http://api.yummly.com/v1/api/recipes?_app_id=9fb53b76&_app_key=d2fd726c3304d520c0ede8036c93f010&maxResult=50&q=' + term)
-   .success(function(result){
-	 console.log(result);
-   	 $scope.recipeList = result;
-   });
- }   */ 
 
 /// - searchctrl
