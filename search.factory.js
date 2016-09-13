db88246cf6792ed51f295c463c1d417e23adc2c9
Aleah Manzi <aleah.manzi@gmail.com>
2016-09-13 07:11:08 -0400

    created multiple input fields and began adding checkboxes for meal type.  Started the CSS formatting and placement.

--- search.factory.js
+++ search.factory.js
@@ -1,9 +1,9 @@
 angular.module('myApp')
 
 .factory('search', function ($http){
-	return function(term){
+	return function(block){
 		return $http ({
-			url: 'http://api.yummly.com/v1/api/recipes?_app_id=9fb53b76&_app_key=d2fd726c3304d520c0ede8036c93f010&maxResult=50&q=' + term,
+			url: 'http://api.yummly.com/v1/api/recipes?_app_id=9fb53b76&_app_key=d2fd726c3304d520c0ede8036c93f010&maxResult=50&q=' + block,
 			method: 'JSONP',
 			params: {callback: 'JSON_CALLBACK'}
 		});
--- search.js
+++ search.js
@@ -3,18 +3,43 @@ angular.module('myApp').controller('searchctrl',
 
   $scope.recipeList = [];
   $scope.matches = [];
+  $scope.term = {
+   ingredient1: '',
+   ingredient2: '',
+   ingredient3: '',
+   ingredient4: '',
+   ingredient5: ''
+   };
+  $scope.block = {
+   exclude1: '',
+   exclude2: '',
+   exclude3: '',
+   exclude4: '',
+   exclude5: ''
+   };
+  $scope.meal = {
+   option1: '',
+   option2: '',
+   option3: '',
+   option4: '',
+   option5: ''
+   };
   /// - default values
-
-   $scope.search = function(){
-    console.log($scope.term);
-     search($scope.term).success(function(result){
+  
+ $scope.search = function(){
+    console.log($scope.block);
+     search($scope.block).success(function(result){
       console.log(result);
       $scope.matches = result.matches;
      })
-   };
+  };
+
+
+
+
+
 
 });
-
 
 
 /// - searchctrl
