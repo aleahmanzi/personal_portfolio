6d5ff5607a353e215148fa203424642af65c8c75
Aleah Manzi <aleah.manzi@gmail.com>
2016-10-11 20:49:33 -0400

    fixing "issues"

--- js/config.js
+++ js/config.js
@@ -4,19 +4,15 @@ angular.module('myApp')
     $routeProvider
     .when('/', {
         templateUrl: './html/home.html',
-        controller: 'homectrl'
     })
     .when('/about', {
         templateUrl: './html/about.html',
-        controller: 'about'
     })
     .when('/portfolio', {
         templateUrl: './html/portfolio.html',
-        controller: 'portfolio'
     })
     .when('/contact', {
         templateUrl: './html/contact.html',
-        controller: 'contact'
     })
     .when('/error', {
         template: '<p>page not found</p>'
