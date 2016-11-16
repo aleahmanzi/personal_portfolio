17afb376f4cfab5b2ee02f8909eeea3c1938bc74
Aleah Manzi <aleah.manzi@gmail.com>
2016-11-15 20:27:05 -0500

    adding back deleted html folder

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
-angular.module('myApp',['ngRoute', 'ui.bootstrap', 'ngAnimate' ]);
+var myApp = angular.module('myApp', []);
+	myApp.controller('ctrl', function ($scope, $location, $http, $sce, $anchorScroll){
 
+$scope.aleah = "hello there"      
+
+		$scope.gotoAbout = function() {
+      	console.log("hello");
+        $location.hash('about');
+        $anchorScroll();
+      };
+
+      $scope.meMe = function(){
+      	console.log("hi there");
+      }
+ }); /// - ctrl
---
+++ js/config.js
@@ -0,0 +1,20 @@
+angular.module('myApp') 
+
+ .config(['$routeProvider', function($routeProvider){
+    $routeProvider
+    .when('/', {
+        templateUrl: './html/home.html',
+    })
+    .when('/about', {
+        templateUrl: './html/about.html',
+    })
+    .when('/portfolio', {
+        templateUrl: './html/portfolio.html',
+    })
+    .when('/contact', {
+        templateUrl: './html/contact.html',
+    })
+    .when('/error', {
+        template: '<p>page not found</p>'
+    });
+ }]); /// - config
