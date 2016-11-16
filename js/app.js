6ab3fac91594509257f6922f91e521aa992ca31e
Aleah Manzi <aleah.manzi@gmail.com>
2016-11-15 22:13:48 -0500

    updating portfolio page to be single column

--- js/app.js
+++ js/app.js
@@ -1,15 +1,2 @@
-var myApp = angular.module('myApp', []);
-	myApp.controller('ctrl', function ($scope, $location, $http, $sce, $anchorScroll){
+angular.module('myApp',['ngRoute', 'ui.bootstrap'])
 
-$scope.aleah = "hello there"      
-
-		$scope.gotoAbout = function() {
-      	console.log("hello");
-        $location.hash('about');
-        $anchorScroll();
-      };
-
-      $scope.meMe = function(){
-      	console.log("hi there");
-      }
- }); /// - ctrl
